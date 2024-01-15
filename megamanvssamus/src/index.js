8a7557dc2e3afe26c2382795bb2b9f67039086d5
Caleb Cook <bmxcaleb@gmail.com>
2024-01-15 15:46:43 -0600

    Start Menu start

--- megamanvssamus/src/index.js
+++ megamanvssamus/src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { App, App2 } from 'components';
+import { App } from 'components';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <App2 />
   </React.StrictMode>,
 );
 
