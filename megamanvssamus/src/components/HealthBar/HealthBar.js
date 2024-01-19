a204540727e7a79ae3a3f69acf5b2966b8663c22
Caleb Cook <bmxcaleb@gmail.com>
2024-01-19 16:01:48 -0600

    fixing health bar

--- megamanvssamus/src/components/HealthBar/HealthBar.js
+++ megamanvssamus/src/components/HealthBar/HealthBar.js
@@ -4,7 +4,7 @@ export const HealthBar = ({ label, value, maxValue }) => {
   return (
     <div className={styles.main}>
       <div className={styles.label}>{label}</div>
-      <div classname={styles.max}>
+      <div className={styles.max}>
         <div
           className={styles.value}
           style={{ width: `${(value / maxValue) * 100}%` }}
